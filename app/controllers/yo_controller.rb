350d3e502e9faac405d4aee311c39068c4189235
Shawn Presser <shawnpresser@gmail.com>
2017-04-19 18:12:58 -0500

    Add /yo(.json)

--- app/controllers/yo_controller.rb
+++ app/controllers/yo_controller.rb
@@ -1,2 +1,8 @@
 class YoController < ApplicationController
+  def index
+    respond_to do |format|
+      format.json { render json: '"yo"' }
+      format.html { render html: "yo" }
+    end
+  end
 end
