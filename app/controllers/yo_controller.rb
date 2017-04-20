eed3eb6bd376a036df85a2e014e9f6e55319b0ee
Shawn Presser <shawnpresser@gmail.com>
2017-04-19 19:03:50 -0500

    Add name parameter to /yo

--- app/controllers/yo_controller.rb
+++ app/controllers/yo_controller.rb
@@ -1,8 +1,11 @@
 class YoController < ApplicationController
   def index
+    l = ["yo"]
+    l.push(params[:name]) if params.key?(:name)
+
     respond_to do |format|
-      format.json { render json: '"yo"' }
-      format.html { render html: "yo" }
+      format.json { render json: { response: "ok", message: l.join(" ").to_json }.to_json }
+      format.html { render html: l.join(" ") }
     end
   end
 end
